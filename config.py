59ed48038c7d1f77f6411d2eb2c644b6209db67a
Nathan Patten <nrpatten@gmail.com>
2015-07-14 13:15:59 +1000

    [FIX] Typo

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ COINDAEMON_TRUSTED_PASSWORD = 'mpostestnet'
 # scrypt, sha256d, scrypt-jane, skeinhash, quark and riecoin
 # If the option does not meet either of these criteria stratum defaults to scrypt
 # For Coins which support TX Messages please enter yes in the TX selection
-COINDAEMON_ALGO = 'script'
+COINDAEMON_ALGO = 'scrypt'
 COINDAEMON_TX = 'no'
 
 # ******************** BASIC SETTINGS ***************
